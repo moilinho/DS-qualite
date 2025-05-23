ea4854248646798fe9e4b9ee6c9f4a1838246863
moilinho <moilim.abdallah@edu.univ-paris13.fr>
2025-05-23 11:52:48 +0200

    suppression d'un import redondant

--- CalculatriceTest.java
+++ CalculatriceTest.java
@@ -2,7 +2,7 @@ package com.example.demo.data;
 
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 public class CalculatriceTest {
 
